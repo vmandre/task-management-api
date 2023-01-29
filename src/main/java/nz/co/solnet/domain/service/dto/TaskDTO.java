b63a68be4dd0d87a28811fe48a76b3cba67d4ff3
vmandre <vmandre@gmail.com>
2023-01-30 00:57:30 +1300

    Add lombok annotation for no args constructor

--- src/main/java/nz/co/solnet/domain/service/dto/TaskDTO.java
+++ src/main/java/nz/co/solnet/domain/service/dto/TaskDTO.java
@@ -3,11 +3,13 @@ package nz.co.solnet.domain.service.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TaskDTO {
 
     private Integer id;
